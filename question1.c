4588ca0f4b557ffc005e7e86f139af96ab544f2b
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-03-28 20:42:22 -0300

    feat: `Question 1`

---
+++ question1.c
@@ -0,0 +1,14 @@
+// 1. Ler um número e calcular o dobro
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    printf("O dobro de %d é %d\n", num, num * 2);
+
+    return 0;
+}
