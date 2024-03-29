bbbad2d82888f7ef6f50820df80775a060108735
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-03-29 16:26:52 -0300

    feat: `Question 6`

---
+++ question6.c
@@ -0,0 +1,29 @@
+// 6. Ler três números e escrever a diferença entre o maior e o menor valor lido.
+
+#include <stdio.h>
+
+int main() {
+    int min, max;
+
+    printf("Digite os números:\n");
+    scanf("%d", &min);
+    max = min;
+
+    for (int i = 0; i < 2; i++) {
+        int num;
+
+        scanf("%d", &num);
+
+        if (num < min) {
+            min = num;
+        } else if (num > max) {
+            max = num;
+        }
+    }
+
+    int difference = max - min;
+
+    printf("A diferença entre o maior e menor é %d\n", difference);
+
+    return 0;
+}
