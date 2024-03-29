88afa383665755faaf2374d7e056da277a82084a
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-03-29 16:39:31 -0300

    feat: `Question 10`

---
+++ question10.c
@@ -0,0 +1,11 @@
+// 10. Escrever todos os números ímpares de 1 a 50.
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i < 50; i += 2) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
