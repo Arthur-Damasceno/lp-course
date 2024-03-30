c13de00c56e89b9349cf9ce36b3337baee2a92af
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-03-30 09:38:06 -0300

    feat: `Question 11`

---
+++ question11.c
@@ -0,0 +1,11 @@
+// 11. Escrever os quadrados dos números entre 1000 e 2000.
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 1000; i <= 2000; i++) {
+        printf("%d\n", i*i);
+    }
+
+    return 0;
+}
