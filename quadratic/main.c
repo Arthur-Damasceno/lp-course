455b1537b1d83331279452ad5bd6c28302781f9d
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-04-09 15:12:37 -0300

    feat: `quadratic` question 1 - part 1

---
+++ quadratic/main.c
@@ -0,0 +1,35 @@
+// Parte 1 Questão 1. Dados os coeficientes de uma função do 2° grau,
+// encontrar as raízes reais da função.
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c;
+
+    printf("Digite os coeficientes da função em ordem decrescente de grau: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if (a == 0) {
+        printf("O coeficiente a não pode ser nulo!\n");
+        return 1;
+    }
+
+    double delta = pow(b, 2) -4*a*c;
+
+    if (delta < 0) {
+        printf("A função não possui raízes reais!\n");
+    } else if (delta == 0) {
+        double root = -b / (2*a);
+
+        printf("A raiz da função é %lf\n", root);
+    } else {
+        double sqrtDelta = sqrt(delta);
+        double root1 = (-b + sqrtDelta) / (2*a);
+        double root2 = (-b - sqrtDelta) / (2*a);
+
+        printf("As raízes são %lf e %lf\n", root1, root2);
+    }
+
+    return 0;
+}
